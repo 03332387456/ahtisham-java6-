2910dbf61b0c447e309a3c1ce2ce8eb2d2201fce
03332387456 <sahirmavia@gmail.com>
2023-05-24 12:37:47 +0500

    work completed

---
+++ app.js
@@ -0,0 +1,71 @@
+// ======== Events ============
+
+
+
+// function abc(message) {
+//     alert(message);
+// }
+
+
+
+
+
+
+// function changeImage(img) {
+//     var image2url =
+//         "https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_1280.jpg";
+// img.src = image2url
+// console.log(img);
+//}
+
+
+// ===========change image=============
+// ===========change image=============
+// ===========change image=============
+
+
+// var img1 = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+// var img2 =" https://img.freepik.com/free-photo/space-background-realistic-starry-night-cosmos-shining-stars-milky-way-stardust-color-galaxy_1258-154643.jpg"
+
+// function changeimage1(img) {
+//     img.src = img1
+// }
+// function changeimage2(img) {
+//    img.src = img2
+// }
+
+
+// var input1 = document.getElementById("Firstname1")
+// var input2 = document.getElementById("Firstname2")
+// var input3 = document.getElementById("Firstname3")
+// var input4 = document.getElementById("Firstname4")
+// var disp = document.getElementById("display")
+
+
+// var input5 = document.getElementById("Firstname5")
+// var disp1 = document.getElementById("display1")
+
+// function register1() {
+    // console.log(input1.value);
+    // console.log(input2.value);
+    // console.log(input3.value);
+    // console.log(input4.value);
+    // disp.value = input4.value
+
+    // console.log(input5.value);
+    // disp1.value = input5.value
+
+// }
+
+var input1 = document.getElementById("Firstname")
+var input2 = document.getElementById("Surname")
+var input3 = document.getElementById("Mblno")
+var input4 = document.getElementById("password")
+
+
+function signup() {
+    console.log(input1.value);
+    console.log(input2.value);
+    console.log(input3.value);
+    console.log(input4.value);
+}
